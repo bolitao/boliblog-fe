0545e3dd4f8abbbc7264e07894a22f8b3645fcf1
Boli Tao <tblsyx@outlook.com>
2020-12-27 15:20:02 +0800

    routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,48 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import BlogDetail from '../views/BlogDetail.vue'
+import BlogEdit from '../views/BlogEdit.vue'
+import Blogs from '../views/Blogs.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+    // {
+    //     path: '/',
+    //     name: 'index',
+    //     redirect: {name: 'Blogs'}
+    // },
+
     {
         path: '/',
-        name: 'Home',
-        component: Home
+        name: 'Blogs',
+        component: Blogs
     },
+
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+
+    {
+        path: '/blog/:blogId/edit',
+        name: 'BlogEdit',
+        component: BlogEdit
+    },
+
+    {
+        path: '/blog/add',
+        name: 'BlogAdd', // TODO: blogEdit or blogAdd
+        component: BlogEdit
+    },
+
+    {
+        path: '/blog/:blogId',
+        name: 'BlogDetail',
+        component: BlogDetail
+    },
 ]
 
 const router = new VueRouter({
