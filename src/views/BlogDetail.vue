db67f5127bce518d13c701d49ecafd31c4b863bc
Boli Tao <tblsyx@outlook.com>
2021-02-12 19:11:15 +0800

    blog detail

--- src/views/BlogDetail.vue
+++ src/views/BlogDetail.vue
@@ -1,13 +1,56 @@
 <template>
-  <div>detail</div>
+  <div>
+    <Header/>
+    <div class="mblog">
+      <h2>{{ blog.title }}</h2>
+      <el-link icon="el-icon-edit">
+        <router-link :to="{name: 'BlogEdit', params: {blogId: blog.id}}">编辑</router-link>
+      </el-link>
+      <el-divider/>
+      <div class="markdown-body" v-html="blog.content"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import "github-markdown-css"
+
 export default {
-  name: "BlogDetail"
+  name: "BlogDetail",
+  components: {Header},
+  created() {
+    const blogId = this.$route.params.blogId;
+    if (blogId) {
+      const _this = this;
+      this.$axios.get('/blogs/' + blogId).then(res => {
+        const blog = res.data.data;
+        _this.blog.id = blog.id;
+        _this.blog.title = blog.title;
+
+        let MarkdownIt = require('markdown-it');
+        let md = new MarkdownIt();
+        _this.blog.content = md.render(blog.content);
+      });
+    }
+  },
+  data() {
+    return {
+      blog: {
+        id: '',
+        title: 'default',
+        content: 'default_content'
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.mblog {
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  width: 100%;
+  min-height: 700px;
+  padding: 20px 10px;
+}
 </style>
